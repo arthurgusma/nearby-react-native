c89534745dc6c57617335cf00fed2420fefd8a8d
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-12-10 10:46:08 -0300

    fix: tscofig de path

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from "expo-router";
-import { colors } from "@//styles/theme";
+import { colors } from "@/styles/theme";
 
 import { 
     useFonts, 
--- src/components/loading/styles.ts
+++ src/components/loading/styles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { colors } from "@//styles/theme";
+import { colors } from "@/styles/theme";
 
 export const styles = StyleSheet.create({
     container: {
